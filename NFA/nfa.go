431c80cead8ae793bac5ac1ca6460a693fe0aae5
ika25 <lomiika1990@yahoo.com>
2018-03-28 16:39:15 +0100

    Implementing shuting yard algorithm

--- NFA/nfa.go
+++ NFA/nfa.go
@@ -1,1 +1,32 @@
+package main
 
+import (
+
+	"fmt"
+	"strconv"
+
+	//thompsons "./thompsons"
+	//utils "./utils"
+)
+
+func shunt(infix string) string { //function to convert inflix regExp to postfix regExp
+
+	specials := map[rune]int{'*': 10, '.': 9, '|': 8} //create a map for special characters which will be used in this program
+
+	pofix := []rune{}
+	s := []rune{}
+
+
+	for _, r := range infix {
+		switch {
+		case r == "(":
+			s = append (s,r)
+
+		case r == ")":
+			for s[len(s)-1] != "("{
+				postfix = append(psotfix, s [len(s)-1])
+				s = s[:len(s)-1]
+			}	
+		}
+	
+}
