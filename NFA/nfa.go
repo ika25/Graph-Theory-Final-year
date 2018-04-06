8bf9aa385bbae3d449c9506937642494e06000dc
ika25 <lomiika1990@yahoo.com>
2018-04-06 14:01:38 +0100

    nfa states done

--- NFA/nfa.go
+++ NFA/nfa.go
@@ -1,49 +1,18 @@
 package main
+import ("fmt")
 
-import "fmt"
 
-//thompsons "./thompsons"
-//utils "./utils"
-
-func shunt(infix string) string { //function to convert inflix regExp to postfix regExp
-
-	specials := map[rune]int{'*': 10, '.': 9, '|': 8} //create a map for special characters which will be used in this program
-
-	pofix := []rune{}
-	s := []rune{}
-
-	for _, r := range infix {
-		switch {
-		case r == "(":
-			s = append(s, r)
-
-		case r == ")":
-			for s[len(s)-1] != "(" {
-				postfix = append(psotfix, s[len(s)-1])
-				s = s[:len(s)-1]
-			}
-
-			s = s[:len(s)-1]
-
-		case prec[r] > 0:
-			for len(s) > 0 && prec[r] <= prec[s[len(s)-1]] {
-				postfix = append(pofix, s[len(s)-1])
-				s = s[:len(s)-1]
-			}
-
-			s = append(s, r)
-		default:
-			postfix = append(postfix, r)
-		}
-
-	}
-
-	for len(s) > 0 {
-		postfix = append(postfix, s[len(s)-1])
-		s = s[:len(s)-1]
-	}
-
-	return string(postfix)
+type state struct {
+    symbol rune
+    edge1 *state
+    edge2 *state
 }
 
+func poregtonenfa(pofix string){
 
+}
+
+func main(){
+	nfa := poregtonenfa("ab.c*|")
+	fmt.Println(nfa)
+}
